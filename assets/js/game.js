1e439ca5b99b3882b981e7fa4a69df2870ddce34
mtaycruz <mtaycruz@penguin>
2020-12-14 17:06:03 -0500

    exploring with javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,16 @@
-var name = "your name";
-console.log(name);
+var playerName = window.prompt("What is your robots name");
+
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+  window.alert("Welcome to robot Gladiators");
+}
+
+fight();
